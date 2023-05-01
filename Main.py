b80a748309b7bf7654915a9d8f642d6e42bdd510
Dylan Dong <61720063+ddong1230@users.noreply.github.com>
2023-05-01 17:29:57 -0400

    update scrape.py

--- Main.py
+++ Main.py
@@ -110,7 +110,7 @@ class App(customtkinter.CTk):
     def open_web(self,keyword):
         webbrowser.open_new("https://www.iyf.tv/search/"+keyword)
 
-    def get_img(self,url,x=100,y =100):
+    def get_img(self, url, x = 100, y = 100):
         image = customtkinter.CTkImage(Image.open(requests.get(url, stream=True).raw), size=(x, y))
         return image
     
@@ -119,13 +119,12 @@ class App(customtkinter.CTk):
         count = 0
         self.anime_today = dict()
         for anime in self.anime_list:
-            if self.day_of_week == anime[1]:
-                #initialize the buttons and connect callback function to open relative webpage. 
-                self.anime_today[count]=customtkinter.CTkButton(self.home_buttons_frame, text=anime[0], 
-                                                           image=self.get_img(anime[3]), compound="top",
-                                                           command=lambda a = anime[0]: self.open_web(a ))
-                self.anime_today[count].grid(row=count, column=0, padx=20, pady=10)
-                count +=1
+            #initialize the buttons and connect callback function to open relative webpage. 
+            self.anime_today[count]=customtkinter.CTkButton(self.home_buttons_frame, text=anime[0], 
+                                                        image=self.get_img(anime[3]), compound="top",
+                                                        command=lambda a = anime[0]: self.open_web(a))
+            self.anime_today[count].grid(row=count, column=0, padx=20, pady=20)
+            count += 1
     
 
     def select_frame_by_name(self, name):
--- scrape.py
+++ scrape.py
@@ -8,22 +8,31 @@ headers = {
     }
 
 weekday = {
-    'Monday': 1,
-    'Tuesday': 2,
-    'Wednesday': 3,
-    'Thursday': 4,
-    'Friday': 5,
-    'Saturday': 6,
-    'Sunday': 7,
+    'Monday': 0,
+    'Tuesday': 1,
+    'Wednesday': 2,
+    'Thursday': 3,
+    'Friday': 4,
+    'Saturday': 5,
+    'Sunday': 6,
+    
+    '一': 0,
+    '二': 1,
+    '三': 2,
+    '四': 3,
+    '五': 4,
+    '六': 5,
+    '日': 6
 }
 
+# Get current date
 now = datetime.datetime.now()
 curr_year = str(now.year)
 curr_month = now.month
-
-# Current season of anime: Jan, Apr, Jul, Oct 
+curr_day = now.weekday()
+print(curr_day)
+# Spring, summer, fall, winter season
 def get_curr_season():
-
     if curr_month >= 1 and curr_month <= 3:
         curr_season = curr_year + '01'
     elif curr_month >= 4 and curr_month <= 6:
@@ -36,29 +45,53 @@ def get_curr_season():
     return curr_season
 
 
-def get_anime():
+def anime_chs():
     anime_list = []
-    
-    # Request from yuc.wiki
     url = 'https://yuc.wiki/{}/'.format(get_curr_season())
     page_text = requests.get(url=url, headers=headers).content
     soup = BeautifulSoup(page_text, 'html.parser')
     animes = soup.find_all('div', {'style':'float:left'})
     
-    
     for anime in animes:
         try:
             name = anime.find('td', {'class':'date_title'}).text
             date = anime.find('p', {'class':'imgtext'}).text[:-1] + '/' + curr_year
+            date = datetime.datetime.strptime(date, "%m/%d/%Y")
+            day = weekday[date.strftime("%A")]
             time = anime.find('p', {'class':'imgep'}).text[:-1]
             img = anime.find('img')['src']
             
-            # Convert date to day of week
-            date = datetime.datetime.strptime(date, "%m/%d/%Y")
-            day = weekday[date.strftime("%A")]
-            
             anime_list.append([name, day, time, img])
         except:
-            pass
+            pass 
     
     return anime_list
+
+def anime_cht():
+    anime_list = []
+    url = 'https://acgsecrets.hk/bangumi/{}/'.format(get_curr_season())
+    page_text = requests.get(url=url, headers=headers).content
+    soup = BeautifulSoup(page_text, 'html.parser')
+    content = soup.find('div', {'id':'acgs-anime-icons'})
+    animes = content.find_all('div', recursive=False)
+    
+    for anime in animes:
+        name = anime.find('div', {'class':'anime_name'}).text
+        day = weekday[anime.find_all('div', {'class':'day'})[1].text]
+        time = anime.find_all('div', {'class':'time'})[1].text
+        img = anime.find('img', {'class':'img-fit-cover'})['src']
+        
+        anime_list.append([name, day, time, img])
+
+    return anime_list
+
+def get_anime(lang = 'chs'):
+    # Determine language of anime list
+    if lang == 'chs':
+        anime_list = anime_chs()
+    elif lang == 'cht':
+        anime_list = anime_cht()
+    elif lang == 'eng':
+        pass
+    
+    return [list for list in anime_list if list[1] == curr_day]
